ed629ec2dc21e9ed5d4335a68aa5224d2507b8db
madjin <32600939+madjin@users.noreply.github.com>
2023-04-28 13:54:27 -0400

    fix

--- scripts/generate_html_gallery.sh
+++ scripts/generate_html_gallery.sh
@@ -1,85 +1,54 @@
 #!/bin/bash
 
+# Function to process files
+process_files() {
+    # Check for required arguments
+    if [[ $# -ne 5 ]]; then
+        echo "Usage: $0 directory filetype output_type output_width output_height"
+        exit 1
+    fi
 
-# Function to print HTML header
-print_html_header() {
-    echo "<!DOCTYPE html>"
-    echo "<html>"
-    echo "<head>"
-    echo "<meta charset=\"UTF-8\">"
-    echo "<title>Image Gallery</title>"
-    echo "<style>"
-    echo "  .gallery {"
-    echo "    display: grid;"
-    echo "    grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));"
-    echo "    grid-gap: 10px;"
-    echo "  }"
-    echo "  .gallery img {"
-    echo "    width: 100%;"
-    echo "    height: 100%;"
-    echo "    object-fit: cover;"
-    echo "  }"
-    echo "</style>"
-    echo "</head>"
-    echo "<body>"
-    echo "<div class=\"gallery\">"
-}
-
-# Function to generate HTML gallery
-generate_html_gallery() {
     local dir="$1"
+    local filetype="$2"
+    local output_type="$3"
+    local output_width="$4"
+    local output_height="$5"
 
-    ## Add ability to find gif if thumbnails become animated in future
-    find "$dir" -type f -name "*.png" | while read -r file; do
-        local glb_file="${file%.*}.glb"
-        echo "  <a href="$glb_file"><img src="$file"></a>"
+    # Loop through all files of given filetype in the directory
+    find "$dir" -type f -name "*.$filetype" -print0 | while IFS= read -r -d '' file; do
+        echo "Processing $file..."
+        local base_filename=$(basename "$file" ".$filetype")
+        local skip_gif=false skip_png=false
+
+        # Store the directory path of the current file
+        local output_dir=$(dirname "$file")
+
+        [[ -f "$output_dir/$base_filename.gif" ]] && skip_gif=true
+        [[ -f "$output_dir/$base_filename.png" ]] && skip_png=true
+
+        # Process gif files
+        if [[ $output_type == "gif" || $output_type == "both" ]]; then
+            if [[ $skip_gif == false ]]; then
+                for i in {0..5}; do
+                    local yaw=$((i 45 % 315))
+                    local filename="$base_filename-$i.png"
+                    node ./node_modules/.bin/screenshot-glb -i "$file" -w 512 -h 512 -m "orientation=0 0 $yaw" -o "$output_dir/$filename"
+                done
+
+                montage "$output_dir/$base_filename"-*.png -tile 6x1 -geometry +0+0 -background none -resize 3072x512 "$output_dir/montage-$base_filename.png"
+                convert -dispose background -delay 50 -loop 0 "$output_dir/$base_filename"-*.png "$output_dir/$base_filename.gif"
+                rm "$output_dir/$base_filename"-*.png
+            fi
+        fi
+
+        # Process png files
+        if [[ $output_type == "png" || $output_type == "both" ]]; then
+            if [[ $skip_png == false ]]; then
+                node ./node_modules/.bin/screenshot-glb -i "$file" -w "$output_width" -h "$output_height" -m "orientation=0 0 180" -o "$output_dir/$base_filename.png"
+            fi
+        fi
     done
 }
 
-# Main function
-main() {
-    local dir="$1"
-    local print_header=false
-    local print_footer=false
-    local print_body=false
-
-    # Check if optional flag is passed
-    while [[ $# -gt 0 ]]; do
-        case "$2" in
-            --head)
-                print_header=true
-                shift
-                ;;
-            --body)
-                print_body=true
-                shift
-		;;
-            --tail)
-                print_footer=true
-                shift
-                ;;
-            *)
-                break
-                ;;
-        esac
-    done
-
-    # Generate HTML gallery
-    if [[ $print_header == true ]]; then
-	    print_html_header
-    fi
-
-    if [[ $print_body == true ]]; then
-	    generate_html_gallery "$dir"
-    fi
-
-    # Print closing HTML tags if --tail flag is passed
-    if [[ $print_footer == true ]]; then
-      echo "</div>"
-      echo "</body>"
-      echo "</html>"
-    fi
-}
-
-main "$@"
-
+# Call process_files function with command line arguments
+process_files "$@"
